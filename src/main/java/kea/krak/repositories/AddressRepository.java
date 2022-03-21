a753585b5782dd054e30055e87e79e14b0441569
Thony <thon0101@stud.kea.dk>
2022-03-21 09:38:49 +0100

    search: Dummydata fra hard kodet data kan vises via søgefeltet på frontend.

--- src/main/java/kea/krak/repositories/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package kea.krak.repositories;
-
-import kea.krak.entities.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository <Address, Integer> {
-}
--- src/main/java/kea/krak/services/AddressService.java
+++
@@ -1,8 +0,0 @@
-package kea.krak.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AddressService {
-
-}
